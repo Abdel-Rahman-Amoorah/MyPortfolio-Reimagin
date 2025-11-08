8eace8f513e6a9f1ab3a2e2bdead08f034ff2409
Abdel-Rahman-Amoorah <abdulrhmanammourah@gmail.com>
2025-11-08 20:50:20 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/MyPortfolio",
-  assetPrefix: "/MyPortfolio",
   images: { unoptimized: true },
   trailingSlash: true,
   
